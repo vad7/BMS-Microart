d3a814fc21c264dcff9056cddb4491cd77df836c
Vadim <vad7@yahoo.com>
2021-03-20 17:19:31 +0300

    upd text

--- BMS.ino
+++ BMS.ino
@@ -47,8 +47,8 @@ const uint8_t BMS_Cmd_Request[] PROGMEM = { 0x55, 0xAA, 0x01, 0xFF, 0x00, 0x00, 
 
 #ifdef DEBUG_TO_SERIAL
 #ifndef DebugSerial
-#include <SoftwareSerial.h>
-SoftwareSerial DebugSerial(3, 2); // RX, TX
+#include <SoftwareSerial.h>			// Grab all PCINTx interrupts!
+SoftwareSerial DebugSerial(3, 2); 	// RX, TX
 #else
 #undef BMS_SERIAL
 #endif
